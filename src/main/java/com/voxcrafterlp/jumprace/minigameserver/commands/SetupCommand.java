6f4e52c92108d6f8f51b554b9a052e3059de0d64
VoxCrafterLP <voxcrafterlp@gmail.com>
2021-08-08 02:06:58 +0200

    Close #54

--- src/main/java/com/voxcrafterlp/jumprace/minigameserver/commands/SetupCommand.java
+++ src/main/java/com/voxcrafterlp/jumprace/minigameserver/commands/SetupCommand.java
@@ -1,12 +1,18 @@
 package com.voxcrafterlp.jumprace.minigameserver.commands;
 
+import com.google.common.collect.Lists;
 import com.voxcrafterlp.jumprace.JumpRace;
 import com.voxcrafterlp.jumprace.minigameserver.setup.objects.MapSetup;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * This file was created by VoxCrafter_LP!
@@ -15,7 +21,7 @@ import org.bukkit.entity.Player;
  * Project: JumpRace
  */
 
-public class SetupCommand implements CommandExecutor {
+public class SetupCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
@@ -65,5 +71,43 @@ public class SetupCommand implements CommandExecutor {
         player.sendMessage(JumpRace.getInstance().getLanguageLoader().getTranslationByKeyWithPrefix("setup-help-2"));
         player.sendMessage(JumpRace.getInstance().getLanguageLoader().getTranslationByKeyWithPrefix("setup-help-3"));
         player.sendMessage(JumpRace.getInstance().getLanguageLoader().getTranslationByKey("setup-help-4"));
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String label, String[] args) {
+
+        if(!(commandSender instanceof Player)) return Collections.emptyList();
+        final Player player = (Player) commandSender;
+
+        if(command.getName().equalsIgnoreCase("setup")) {
+
+            if(!player.hasPermission(JumpRace.getInstance().getJumpRaceConfig().getSetupPermission())
+                    && !player.hasPermission(JumpRace.getInstance().getJumpRaceConfig().getAdminPermission()))
+                return Collections.emptyList();
+
+            final List<String> completions = Lists.newCopyOnWriteArrayList();
+
+            int length = args.length;
+
+            for(String string : args) {
+                if(string.equals("") || string.equals(" "))
+                    length--;
+            }
+
+            switch (length) {
+                case 0:
+                    completions.add("lobby");
+                    completions.add("map");
+                    break;
+                case 1:
+                    final String search = args[0].toLowerCase();
+                    Stream.of("lobby", "map")
+                            .filter(string -> string.toLowerCase().startsWith(search))
+                            .forEach(completions::add);
+                    break;
+            }
+            return completions;
+        }
+        return Collections.emptyList();
     }
 }
